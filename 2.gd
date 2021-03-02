5c7f55d5fc6aec9258248db6c8565285a264e5cb
lilithkander <78416861+lilithkander@users.noreply.github.com>
2021-03-02 12:36:36 +0200

    Delete 2.gd

--- 2.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _ready():
-	var nimed = ["Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin","Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins","Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey","Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"]
-	
-	var suurus = nimed.size()
